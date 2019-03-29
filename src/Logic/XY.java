651f446aecfc91e9a6f960df4ed7b197944e4875
mac641 <master.tww@web.de>
2019-03-29 13:56:09 +0100

    generateNewPosition slenderized

--- src/Logic/XY.java
+++ src/Logic/XY.java
@@ -1,7 +1,5 @@
 package Logic;
 
-import javax.swing.plaf.ColorUIResource;
-import java.time.temporal.ChronoUnit;
 import java.util.Random;
 
 public final class XY {
@@ -42,71 +40,48 @@ public final class XY {
                 '}';
     }
 
-    //TODO: probably implementation with switch-case
     public XY generateNewPosition(XY currentPosition) {
-        int possiblePosition[][] = new int[2][9];
         Random rd = new Random();
-        int rdNo = rd.nextInt(9);
+        XY out = currentPosition;
 
-        possiblePosition[0][0] = -1;
-        possiblePosition[1][0] = -1;
-        possiblePosition[0][1] = -1;
-        possiblePosition[1][1] = 0;
-        possiblePosition[0][2] = -1;
-        possiblePosition[1][2] = 1;
-        possiblePosition[0][3] = 0;
-        possiblePosition[1][3] = -1;
-        possiblePosition[0][4] = 0;
-        possiblePosition[1][4] = 0;
-        possiblePosition[0][5] = 0;
-        possiblePosition[1][5] = 1;
-        possiblePosition[0][6] = 1;
-        possiblePosition[1][6] = -1;
-        possiblePosition[0][7] = 1;
-        possiblePosition[1][7] = 0;
-        possiblePosition[0][8] = 1;
-        possiblePosition[1][8] = 1;
+        while (true) {
+            int rdNo = rd.nextInt(9);
 
-
-
-
-
-
-        for (int i = 0; i < possiblePosition[1].length; i++) {
-            for (int y = -1; y < 2; y++) {
-                for (int x = -1; x < 2; x++) {
-                    possiblePosition[i].setX(currentPosition.getX() + x);
-                    possiblePosition[i].setY(currentPosition.getY() + y);
-                }
+            switch (rdNo) {
+                case 0:
+                    out.setY(currentPosition.getY() - 1);
+                    out.setX(currentPosition.getX() - 1);
+                    break;
+                case 1:
+                    out.setY(currentPosition.getY() - 1);
+                    break;
+                case 2:
+                    out.setY(currentPosition.getY() - 1);
+                    out.setX(currentPosition.getX() + 1);
+                    break;
+                case 3:
+                    out.setX(currentPosition.getX() - 1);
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    out.setX(currentPosition.getX() + 1);
+                    break;
+                case 6:
+                    out.setY(currentPosition.getY() + 1);
+                    out.setX(currentPosition.getX() - 1);
+                    break;
+                case 7:
+                    out.setY(currentPosition.getY() + 1);
+                    break;
+                case 8:
+                    out.setY(currentPosition.getY() + 1);
+                    out.setX(currentPosition.getX() + 1);
+                    break;
             }
-        }
 
-        if (currentPosition.y == 0 && currentPosition.x == 0){ //top left corner
-
-        }
-        else if (currentPosition.y == 0 && currentPosition.x != 0 && currentPosition.x != xLength) { //space between top left corner and top right corner
-
-        }
-        else if (currentPosition.y == 0 && currentPosition.x == xLength) { //top right corner
-
-        }
-        else if (currentPosition.y != 0 && currentPosition.y != yLength && currentPosition.x == xLength) { // space between top right corner and bottom right corner
-
-        }
-        else if (currentPosition.y == yLength && currentPosition.x == xLength) { //bottom right corner
-
-        }
-        else if (currentPosition.y == yLength && currentPosition.x != 0 && currentPosition.x != xLength) { //space between bottom right corner and bottom left corner
-
-        }
-        else if (currentPosition.y == yLength && currentPosition.x == 0) { // bottom left corner
-
-        }
-        else if (currentPosition.y != 0 && currentPosition.x != yLength && currentPosition.x == 0) { //space between bottom left corner and top left corner
-
-        }
-        else { //everything in between
-
+            if (!(out.getX() < 0 || out.getY() < 0 || out.getX() > xLength || out.getY() > yLength))
+                return out;
         }
     }
 }
