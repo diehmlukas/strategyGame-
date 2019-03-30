d21b2d0a62834f8c4848d7ebf24a20394fb4e383
mac641 <master.tww@web.de>
2019-03-30 09:08:35 +0100

    EntitySet: useless trash code deleted (comments) -> new array implementation should work Main: short test for EntitySet added

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -1,10 +1,13 @@
 package Logic;
 
 import Objects.Entities.EntitySet;
+import Objects.Squirrels.MasterSquirrel;
 
 public class Main {
     public static void main(String[] args) {
         EntitySet entitySet = new EntitySet();
+        MasterSquirrel masterSquirrel1 = new MasterSquirrel();
 
+        entitySet.addEntity(masterSquirrel1);
     }
 }
